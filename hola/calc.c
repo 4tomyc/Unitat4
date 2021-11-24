bff06d3f5255b4f4bd63ae0159a16cee031bdd10
Tomás <tomascastello444@gmail.com>
2021-11-24 13:54:40 +0100

    Añadidas carpetas calc y hola

---
+++ hola/calc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int suma(int a, int b)
+{
+    return a + b;
+}
+
+int resta(int a, int b)
+{
+    return a - b;
+}
+
+int multiplica(int a, int b)
+{
+    return a * b;
+}
+
+int divideix(int a, int b)
+{
+    return a / b;
+}
+
---
+++ hola/calc.h
@@ -0,0 +1,9 @@
+#ifndef MYCALC
+#define MYCALC
+
+int suma(int a, int b);
+int resta(int a, int b);
+int multiplica(int a, int b);
+int divideix(int a, int b);
+
+#endif
